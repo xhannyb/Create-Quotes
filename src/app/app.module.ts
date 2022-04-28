8bfda4065c0985b135ccf7db7d5b1158aa5ef615
xhannyb <shannonbulimo@gmail.com>
2022-04-28 08:50:57 +0300

    Quotes details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HighlightQuotesDirective } from './highlight-quotes.directive';
 
 @NgModule({
   declarations: [
