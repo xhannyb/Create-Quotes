ed94266786a48bcc6a66c88eb9b0c95c5a1d3c80
xhannyb <shannonbulimo@gmail.com>
2022-04-28 08:07:21 +0300

    highlight-quotes.directive.spec.ts

---
+++ src/app/highlight-quotes.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightQuotesDirective } from './highlight-quotes.directive';
+
+describe('HighlightQuotesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightDirective();
+    expect(directive).toBeTruthy();
+  });
+});
